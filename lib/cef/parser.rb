22d7be09199287f37a9a85f8e2210858995400e9
Ryan Breed <opensource@breed.org>
2011-03-29 05:24:03 -0500

    started the parser

--- lib/cef/parser.rb
+++ lib/cef/parser.rb
@@ -1,13 +1,24 @@
 # COPYRIGHT: Ryan Breed
 # DATE:      3/27/11
-class Parser
-  # attr_accessor :foo
+module CEF
+  class Parser
+    attr_accessor :file_name
 
-  def initialize(*args)
-    # Parser.new(:foo=>"bar)
-    Hash[*args].each { |argname, argval| self.send(("%s="%argname), argval) }
+    def initialize(*args)
+      # Parser.new(:foo=>"bar)
+      Hash[*args].each { |argname, argval| self.send(("%s="%argname), argval) }
+      yield self if block_given?
+    end
+    def parse_file
+      File.open(file_name) do |f|
+        f.each_line do |line|
+          line.chomp!
+          prefix=line.split(/[^\\]\|/).map {|i| i.strip}
+          extension=Hash[ *prefix_components[-1].split(/([\w]+)=/).select {|i| i!=""}.map {|i| i.strip} ]
+          
+        end
+      end
+    end
 
-
-    yield self if block_given?
   end
 end
