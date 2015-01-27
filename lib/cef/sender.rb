577f9fddf723fc2439e602ea392e0eced19e3058
Igal Kreichman <igal@skycure.com>
2015-01-27 19:44:30 +0200

    Add reciever host and port in initializer of UDPSender

--- lib/cef/sender.rb
+++ lib/cef/sender.rb
@@ -13,6 +13,10 @@ module CEF
   #TODO: Implement relp/tcp senders
 
   class UDPSender < Sender
+    def initialize(receiver='127.0.0.1', port=514)
+      @receiver = receiver
+      @port = port
+    end
 
     #fire the message off
     def emit(event)
@@ -27,12 +31,9 @@ module CEF
       self.sock.send event.to_s, 0
     end
 
-
     def socksetup
       @sock=UDPSocket.new
-      receiver= self.receiver || "127.0.0.1"
-      port= self.receiverPort || 514
-      @sock.connect(receiver,port)
+      @sock.connect(@receiver, @port)
     end
   end
 end
--- spec/lib/cef/sender_spec.rb
+++ spec/lib/cef/sender_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 describe CEF::UDPSender do
+  it 'defaults receiver to localhost on port 514' do
+    sock_double = double
+    expect(UDPSocket).to receive(:new).and_return(sock_double)
+    expect(sock_double).to receive(:connect).with('127.0.0.1', 514)
+
+    sender = CEF::UDPSender.new
+    sender.socksetup
+  end
+
   it 'receives an escaped message when emit is called' do
     event = CEF::Event.new
 
     sock_double = double
     expect(UDPSocket).to receive(:new).and_return(sock_double)
-    expect(sock_double).to receive(:connect).with('127.0.0.1', 514)
+    expect(sock_double).to receive(:connect).with('myDomain.org', 4321)
     expect(sock_double).to receive(:send).with(event.to_s, 0)
 
-    sender = CEF::UDPSender.new
+    sender = CEF::UDPSender.new('myDomain.org', 4321)
     sender.emit(event)
   end
 end
