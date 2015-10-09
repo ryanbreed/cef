51fc221128298df014ef9f0ad7ce1796fdfcf7b4
Ryan Breed <opensource@breed.org>
2015-10-09 10:06:47 -0500

    property decs work

--- lib/cef/event.rb
+++ lib/cef/event.rb
@@ -1,8 +1,25 @@
 module Cef
   class Event < Hashie::Dash
+    include Hashie::Extensions::Coercion
+
     SCHEMA_CONFIG_FILE = File.join(File.expand_path(File.dirname(__FILE__)),'..','..','conf','cef-schema.json')
     config = JSON.parse(File.read(SCHEMA_CONFIG_FILE))
-    config['prefix'].each    {|a| self.class_eval { property a.to_sym, required: true }}
-    config['extension'].each {|a| self.class_eval { property a.to_sym }}
+
+    if ENV['CEF_CONFIG']!=nil && File.exist?(ENV['CEF_CONFIG'])
+      overrides = JSON.parse(File.read(ENV['CEF_CONFIG']))
+      config.merge!(overrides)
+    end
+
+    config['prefix'].each    do |key, default_val| 
+      self.class_eval do
+        # TODO: this is getting lost
+        puts "setting prefix #{key} default #{default_val}"
+        property key.to_sym, { default: default_val
+      end
+    end
+
+    config['extension'].each do |key| 
+      self.class_eval { property key.to_sym }
+    end
   end
 end
