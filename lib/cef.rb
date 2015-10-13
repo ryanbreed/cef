44a528365e360ee9a9c163e7c061a0c1cbf0889a
Ryan Breed <opensource@breed.org>
2015-10-12 21:48:54 -0500

    convenience methods

--- lib/cef.rb
+++ lib/cef.rb
@@ -12,4 +12,21 @@ require 'cef/loggers/cef_syslog_udp'
 
 
 module CEF
+  def self.logger(config: {}, type: CEF::Loggers::SyslogUdp)
+    configuration = case config
+      when String && File.exist?(config)
+        JSON.parse(File.read(config))
+      when Hash
+        config
+      when File,IO
+        JSON.parse(config.read)
+      else
+        fail ArgumentError, "#{config} is not a valid CEF configuration"
+    end
+        
+    type.new(configuration)
+  end
+  def self.event(*args)
+    CEF::Event.new(*args)
+  end
 end
